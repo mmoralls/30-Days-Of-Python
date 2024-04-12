e8e82ff058a4ffd07555f1ed7ccaf044c989aa11
Mark Moralls <mmoralls0@gmail.com>
2024-04-12 17:14:42 -0400

    Update day-3.py
    Line 110 did not print. Suggested change to:
    print('coding in coding for all', 'coding' in 'coding for all')
    
    Now it works:
    coding in coding for all: True

--- 03_Day_Operators/day-3.py
+++ 03_Day_Operators/day-3.py
@@ -107,7 +107,7 @@ print('1 is 1', 1 is 1)                   # True - because the data values are t
 print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
 print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
 print('B in Asabeneh', 'B' in 'Asabeneh') # False -there is no uppercase B
-print('coding' in 'coding for all') # True - because coding for all has the word coding
+print('coding' in 'coding for all') # True - because coding for all has the word coding; changed to 'coding', 'coding' in 'coding for all'
 print('a in an:', 'a' in 'an')      # True
 print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
 
